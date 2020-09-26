48d7b9987b65148fd7d7e85cd62af099d86f6287
ong-osmond <ong.osmond@yahoo.com>
2020-09-26 20:27:14 +0800

    Added gh-pages deployment scripts.

---
+++ build/precache-manifest.4f8c204a137dc730400a40545ecc767e.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d60b9f5a11ffc82a1b10621c87287c62",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "fb77511348d458c69395",
+    "url": "/React-Portfolio/static/css/2.242f24fe.chunk.css"
+  },
+  {
+    "revision": "49329d6825c9ecaf0a88",
+    "url": "/React-Portfolio/static/css/main.9fbd1ee0.chunk.css"
+  },
+  {
+    "revision": "fb77511348d458c69395",
+    "url": "/React-Portfolio/static/js/2.53ee0565.chunk.js"
+  },
+  {
+    "revision": "f2e097a5fa04a5d1b01656a6cd1a1a1a",
+    "url": "/React-Portfolio/static/js/2.53ee0565.chunk.js.LICENSE"
+  },
+  {
+    "revision": "49329d6825c9ecaf0a88",
+    "url": "/React-Portfolio/static/js/main.f4d6b235.chunk.js"
+  },
+  {
+    "revision": "ed2db9984f9f1e943e82",
+    "url": "/React-Portfolio/static/js/runtime-main.fbcb4e55.js"
+  },
+  {
+    "revision": "3c0abdeb2923c2bfb439a318e6d721a3",
+    "url": "/React-Portfolio/static/media/Osmond_Ong_Profile_pic.3c0abdeb.png"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/React-Portfolio/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/React-Portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/React-Portfolio/static/media/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/React-Portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/React-Portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/React-Portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "768998c65ce07675ede8536017d4f6ce",
+    "url": "/React-Portfolio/static/media/image1.768998c6.jpg"
+  },
+  {
+    "revision": "8de6195d8faafe31ac0a842eb0cbe1a9",
+    "url": "/React-Portfolio/static/media/image2.8de6195d.jpg"
+  },
+  {
+    "revision": "28470933ba170d7cd8d5495969722a5d",
+    "url": "/React-Portfolio/static/media/image3.28470933.jpg"
+  },
+  {
+    "revision": "0b37f43494ce5a35f3e1575612048750",
+    "url": "/React-Portfolio/static/media/image4.0b37f434.jpg"
+  },
+  {
+    "revision": "8d16c4aff0e7bd3db1b8bab9619aea9c",
+    "url": "/React-Portfolio/static/media/image5.8d16c4af.jpg"
+  },
+  {
+    "revision": "62b7cc7591147260c113823ded8f6901",
+    "url": "/React-Portfolio/static/media/image6.62b7cc75.jpg"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/React-Portfolio/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/React-Portfolio/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/React-Portfolio/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "c037dbbc0e6790f30e824a50010df5fb",
+    "url": "/React-Portfolio/static/media/ionicons.c037dbbc.svg"
+  }
+]);
